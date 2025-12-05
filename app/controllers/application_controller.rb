3d86eac010492b9b877bfd32f40574fbe32c310e
nanayanami <234893968+nanayanami@users.noreply.github.com>
2025-12-06 01:44:33 +0900

    registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters
+    def configure_permitted_parameters
+        devise_paramater_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
 end
