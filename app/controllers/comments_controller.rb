6f1e496a65869ac2a298211b4fc28c3cf5532bc9
nanayanami <234893968+nanayanami@users.noreply.github.com>
2025-12-10 18:45:37 +0900

    コントローラーまで

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,12 @@ class CommentsController < ApplicationController
         post = Post.find(params[:post_id])
         comment = current_user.comments.new(comment_params)
         comment.post_id = post.id
+        comment.save
         redirect_to post_path(post)
     end
 
     def destroy
-        comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+        Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
         redirect_to post_path(params[:post_id])
     end
 
