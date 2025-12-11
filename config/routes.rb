8dc6650f192cdbc4648eb17cc98b7c0207e6f232
nanayanami <234893968+nanayanami@users.noreply.github.com>
2025-12-12 00:25:48 +0900

    いいね

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
    resources :posts do
     resources :comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
    end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
