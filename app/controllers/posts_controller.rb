1c299f2eb4c148f929b9ad329dff648dcd0aa2f1
nanayanami <234893968+nanayanami@users.noreply.github.com>
2025-12-12 18:59:35 +0900

    controller機能の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
-    post.save
-    redirect_to post_path(post.id)
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
